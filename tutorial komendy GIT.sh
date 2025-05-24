e36b868070bcae73039f866789c487952e29575f
ponczek4 <dsw55986@student.dsw.edu.pl>
2025-05-24 15:29:03 +0200

    Add files via upload

---
+++ tutorial komendy GIT.sh
@@ -0,0 +1,135 @@
+git clone
+
+git commit
+git checkout o/main
+git commit
+
+git fetch
+
+git pull
+
+git fakeTeamwork 2
+git commit
+git pull
+
+git commit 
+git commit 
+git push
+
+git clone
+git fakeTeamwork
+git commit 
+git pull –rebase
+git push
+
+git branch -f main o/main
+git checkout -b feature C2
+git push origin feature
+
+git fetch 
+git rebase o/main side1
+git rebase side1 side2
+git rebase side3 main
+git push
+
+git checkout main
+git pull
+git merge side1
+git merge side2
+git merge side3
+git push
+
+git checkout -b side o/main
+git commit 
+git pull –rebase
+git push
+
+git push origin main
+git push origin foo
+git push origin main^:foo
+git push origin foo:main
+
+
+
+git fetch origin c3:foo
+git fetch origin c6:main
+git checkout foo
+git merge main
+
+git push origin :foo 
+git fetch origin :bar
+
+git pull origin c3:foo
+git pull origin c2:side
+
+ 
+
+git commit 
+git commit
+
+git branch bugfix
+git checkout bugfix
+
+git checkout -b bugfix
+git commit
+git checkout main
+git commit 
+git merge bugfix
+
+git checkout -b bugfix
+git commit
+git checkout main
+git commit
+git checkout bugfix
+git rebase main
+
+git checkout C4
+
+git checkout bugFix^
+
+git branch -f main C6
+git checkout HEAD~1
+git branch -f bugFix HEAD~1
+
+git reset HEAD~1
+git checkout pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7
+
+git rebase -i overHere 
+
+git rebase -i main 
+git rebase bugFix main
+
+git rebase -i HEAD~2
+git commit –amend
+git rebase -i HEAD~2
+git rebase caption main
+
+git checkout main
+git cherry-pick C2
+git commit –amend
+git cherry-pick C3
+
+git tag v1 side~1
+git tag v0 main~2
+git checkout v1
+
+git commit
+
+git rebase main bugFix
+git rebase bugFix side 
+git rebase side another
+git rebase another main
+
+git branch bugWork main^^2^
+
+git checkout one 
+git cherry-pick C4 C3 C2
+
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
+
+
